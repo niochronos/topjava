4eb51a8af55d649671cc5f7d2997ff5c9e915933
niochronos <niochronos@gmail.com>
2017-07-11 20:40:44 +0300

    Implemented method getFilteredWithExceeded in class UsermealsUtil

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * GKislin
@@ -23,13 +23,43 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000));
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
-        return null;
+        List<UserMealWithExceed> result = new ArrayList<>();
+
+        Map<LocalDate, Integer> dateCaloriesMap = getDatesCaloriesMap(mealList);
+
+        for (UserMeal meal : mealList) {
+            if (TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                int calories = dateCaloriesMap.get(meal.getDateTime().toLocalDate());
+                boolean exceed = (calories > caloriesPerDay)? true : false;
+                result.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), calories, exceed));
+            }
+        }
+
+        return result;
+    }
+
+    private static Map<LocalDate, Integer> getDatesCaloriesMap(List<UserMeal> mealList) {
+        Map<LocalDate, Integer> result = new HashMap<>();
+
+        for (UserMeal meal : mealList) {
+            LocalDate localDate = meal.getDateTime().toLocalDate();
+            int calories = meal.getCalories();
+
+            if (result.containsKey(localDate)) {
+                result.put(localDate, result.get(localDate) + calories);
+            }
+            else {
+                result.put(localDate, calories);
+            }
+        }
+
+        return result;
     }
 }
