c07a6da74e705099ed87daf9e8e10e206ec7fa0b
niochronos <niochronos@gmail.com>
2017-07-31 23:20:04 +0300

    Added scripts for init and fill table MEALS

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_meal_seq;
 
 CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_meal_seq START 100000;
 
 CREATE TABLE users
 (
@@ -21,5 +24,15 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  user_id       INTEGER NOT NULL,
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_meal_seq'),
+  dateTime      TIMESTAMP DEFAULT now(),
+  description   VARCHAR NOT NULL,
+  calories      INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
