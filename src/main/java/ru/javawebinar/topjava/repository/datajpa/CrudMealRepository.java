c7531fda886d0680d0ac0565960a7ad8118e2ee2
niochronos <niochronos@gmail.com>
2017-08-14 14:46:00 +0300

    - Done HW05:   - Implemented CrudMealRepository.java   - Implemented DataJpaMealRepositoryImpl.java   - Created Spring Profile for JDBC, JPA, DataJpa in spring-db.xml   - MealServiceTest and UserServiceTest did abstract   - Add MealServiceJDBCTest   - Add MealServiceJPATest   - Add MealServiceDATAJPATest   - Add UserServiceJDBCTest   - Add UserServiceJPATest   - Add UserServiceDATAJPATest

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,30 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    int deleteByIdAndUserId(int id, int userId);
+
+    @Transactional
+    @Override
+    Meal save(Meal meal);
+
+    Meal findOneByIdAndUserId(int id, int userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(int userId);
+
+    List<Meal> findAllByDateTimeBetweenAndUserIdOrderByDateTimeDesc(LocalDateTime startDateTime,
+                                                                 LocalDateTime endDateTime, int userID);
 }
